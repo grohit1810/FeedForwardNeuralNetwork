f64523d1493fdc3ec8d975023577805b07aefa51
grohit1810 <g.rohit1@nuigalway.ie>
2020-02-25 13:21:16 +0000

    Added code

---
+++ read_cifar_data.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pickle
+
+folder = "CIFAR_Dataset"
+train_files = ["data_batch_"+str(i) for i in range(1,6) ]
+# print files
+test_file = "test_batch"
+
+def NormalizeData(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+def convert_rgb_to_grayscale_and_normalize(image_vector):
+	grayscale_vector = []
+	individual_spec_length = int(len(image_vector)/3) 
+	for i in range(individual_spec_length):
+		red_value = image_vector[i]
+		green_value = image_vector[i + individual_spec_length]
+		blue_value = image_vector[i+ (2*individual_spec_length)]
+		# New grayscale image = ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
+		grayscale_value = ((0.3*red_value) + (0.59*green_value) + (0.11*blue_value))
+		grayscale_vector.append(grayscale_value)
+	return NormalizeData(np.asarray(grayscale_vector))
+
+# This function taken from the CIFAR website
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo)
+    return dict
+
+deer_samples, cat_samples = {}, {}
+deer_train_samples, cat_train_samples = [], []
+
+for file in train_files :
+	data = unpickle(folder+"/" + file)
+	for i in xrange(0,len(data['labels'])):
+		if data['labels'][i] == 3:
+			cat_train_samples.append(convert_rgb_to_grayscale_and_normalize(data['data'][i].tolist()))
+		if data['labels'][i] == 4:
+			deer_train_samples.append(convert_rgb_to_grayscale_and_normalize(data['data'][i].tolist()))
+
+# print(len(deer_train_samples), len(cat_train_samples))
+
+# print(deer_train_samples[0],deer_train_samples[0].shape)
+# print(cat_train_samples[0],cat_train_samples[0].shape)
+deer_samples['train'] = deer_train_samples
+cat_samples['train'] = cat_train_samples
+# pickle.dump(pickle.dump(cat_train_samples, open("cat_train_samples.pkl", "wb")))
+# pickle.dump(pickle.dump(deer_train_samples, open("deer_train_samples.pkl", "wb")))
+
+cat_test_samples, deer_test_samples = [], []
+
+test_data = unpickle(folder+"/" + test_file)
+for i in xrange(0,len(test_data['labels'])):
+	if test_data['labels'][i] == 3:
+		cat_test_samples.append(convert_rgb_to_grayscale_and_normalize(test_data['data'][i].tolist()))
+	if test_data['labels'][i] == 4:
+		deer_test_samples.append(convert_rgb_to_grayscale_and_normalize(test_data['data'][i].tolist()))
+
+deer_samples['test'] = deer_test_samples
+cat_samples['test'] = cat_test_samples
+
+pickle.dump(cat_samples, open("cat_samples.pkl", "wb"))
+pickle.dump(deer_samples, open("deer_samples.pkl", "wb"))
---
+++ read_csv_data.py
@@ -0,0 +1,23 @@
+import csv
+import pickle
+import numpy as np
+file = open("circles500.csv")
+index = 0
+data = []
+for line in file :
+	if index == 0:
+		index +=1
+		continue
+	
+	x0,x1,output_class = line.split(',')
+	#print x0,x1,output_class
+	current_row = {}
+	inp = np.asarray([float(x0),float(x1)])
+	out = np.zeros((2,))
+	out[int(output_class)] = 1
+	current_row["inp"] = inp
+	current_row["out"] = int(output_class)
+	data.append(current_row)
+
+print data
+pickle.dump(data, open("data.pkl", "wb"))
